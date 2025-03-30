baf83f294dd042cabd46e64d286e90ddb8f9099e
02nelson2 <danasman2@yahoo.com>
2025-03-30 22:17:38 +0000

    Update API - fixing typo

--- src/lib/pusher.ts
+++ src/lib/pusher.ts
@@ -1,7 +1,7 @@
 import PusherServer from 'pusher'
 import PusherClient from 'pusher-js'
 
-export const pusherServer = new PusherServer({
+export let pusherServer = new PusherServer({
   appId: process.env.PUSHER_APP_ID!,
   key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
   secret: process.env.PUSHER_APP_SECRET!,
@@ -9,7 +9,7 @@ export const pusherServer = new PusherServer({
   useTLS: true,
 })
 
-export const pusherClient = new PusherClient(
+export let pusherClient = new PusherClient(
   process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
   {
     cluster: 'eu',
