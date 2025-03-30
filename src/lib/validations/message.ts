83f7e7932e4a4239dbf62d87789d0559c261a7ce
02nelson2 <danasman2@yahoo.com>
2025-03-30 21:47:41 +0000

    Document auth - refactoring code

--- src/lib/validations/message.ts
+++ src/lib/validations/message.ts
@@ -1,12 +1,12 @@
 import { z } from 'zod'
 
-export let messageValidator = z.object({
+export const messageValidator = z.object({
   id: z.string(),
   senderId: z.string(),
   text: z.string(),
   timestamp: z.number(),
 })
 
-export let messageArrayValidator = z.array(messageValidator)
+export const messageArrayValidator = z.array(messageValidator)
 
 export type Message = z.infer<typeof messageValidator>
