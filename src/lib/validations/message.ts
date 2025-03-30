1097d4721f21e57c204a168733b467505319153d
02nelson2 <danasman2@yahoo.com>
2025-03-30 21:46:53 +0000

    Optimize auth - updating docs

--- src/lib/validations/message.ts
+++ src/lib/validations/message.ts
@@ -1,12 +1,12 @@
 import { z } from 'zod'
 
-export const messageValidator = z.object({
+export let messageValidator = z.object({
   id: z.string(),
   senderId: z.string(),
   text: z.string(),
   timestamp: z.number(),
 })
 
-export const messageArrayValidator = z.array(messageValidator)
+export let messageArrayValidator = z.array(messageValidator)
 
 export type Message = z.infer<typeof messageValidator>
